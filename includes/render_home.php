16cd89b0b50b5540cf28cda1ba2d929121e172dc
Charisse <charisse.ysabel930@gmail.com>
2015-11-10 19:03:16 +0100

    Add file that renders the header of the homepage

---
+++ includes/render_home.php
@@ -0,0 +1,40 @@
+<?php
+	/**
+	 *	render-home.php
+	 *	render function to make the homepage
+	 *
+	 */ 
+
+
+	/**
+     *	RENDER
+     *	Renders template for the homepage, passing in values.
+     */
+    function render($template, $values = [])
+    {
+        // if template exists, render it
+        if (file_exists("../templates/$template"))
+        {
+            // extract variables into local scope
+            extract($values);
+
+            // render header
+            require("../templates/header-homepage.php");
+
+            // render template
+            require("../templates/$template");
+
+            // render footer
+            require("../templates/footer.php");
+        }
+
+        // else err
+        else
+        {
+            trigger_error("Invalid template: $template", E_USER_ERROR);
+        }
+    }
+
+
+
+?>
