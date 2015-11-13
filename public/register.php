003a9ad038e350e44d4a36b80a0c3c411db18532
Charisse <charisse.ysabel930@gmail.com>
2015-11-13 19:37:52 +0100

    Registers the user to the DB

--- public/register.php
+++ public/register.php
@@ -1,9 +1,46 @@
 <?php
-	/**
-	 *	logout.php
-	 *	registers a user to USERS database.
-	 *
-	 */
+	//configuration
+	require("../includes/config.php");
+	require("../includes/render_log.php");
 
+	//if user reached page via GET
+	if($_SERVER["REQUEST_METHOD"] == "GET")
+	{
+		//render form
+		render("register_form.php", ["title" => "Register"]);
+	}
+	// else if user reached page via POST (submitting a form)
+	else if($_SERVER["REQUEST_METHOD"] == "POST")
+	{
+		if(empty($_POST["username"]))
+		{
+			apologize("Please provide a username.");
+		}
+		else if(empty($_POST["password"]))
+		{
+			apologize("Please enter a password");
+		}
+		else if(empty($_POST["confirmation"]))
+		{
+			apologize("Please confirm password");
+		}
+		else if(($_POST["password"]) !== ($_POST["confirmation"]))
+		{
+			apologize("Password do not match. Try again.");
+		}
+
+		$result = query("INSERT INTO users (username, hash) VALUES(?, ?)", $_POST["username"], crypt($_POST["password"]) );
+			if($result === false)
+			{
+				apologize("Registration failed. Username taken.");
+			}
+
+		$rows = query("SELECT LAST_INSERT_ID() AS id");
+		$id = $rows[0]["id"];
+
+		$_SESSION["id"] = $id;
+
+		redirect("dashboard.php");
+	}
 
 ?>
