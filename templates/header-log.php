e8cd5fd667b332ac9a84817dca164e61381c552a
Charisse <charisse.ysabel930@gmail.com>
2015-11-13 19:46:35 +0100

    Add similar div structure from pset7 for easier styling

---
+++ templates/header-log.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<!-- various libraries -->
+		<link href="/css/bootstrap.min.css" rel="stylesheet"/>
+        <link href="/css/bootstrap-theme.min.css" rel="stylesheet"/>
+
+        <!-- CSS -->
+        <link href="/css/log.css" rel="stylesheet"/>
+
+        <!-- fonts -->
+        <link href='https://fonts.googleapis.com/css?family=Ubuntu' rel='stylesheet' type='text/css'>
+        <link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro' rel='stylesheet' type='text/css'>
+
+    	<!-- scripts -->
+    	<script src="/js/jquery-1.11.1.min.js"></script>
+        <script src="/js/jquery.leanModal.min.js"></script>
+        <script src="/js/bootstrap.min.js"></script>
+
+        <?php if (isset($title)): ?>
+            <title>CS50x Balance: <?= htmlspecialchars($title) ?></title>
+        <?php else: ?>
+            <title>CS50x Balance</title>
+        <?php endif ?>
+
+	</head>
+
+	<body>
+        <div class="container">
+
+            <div id="top">
+                <ul>
+	                <li><a href="/"> Home </a></li>
+	                <li><a href="login.php"> Sign In </a></li>
+	                <li><a href="register.php"> Register </a></li>
+	            </ul>
+            </div>
+
+            <div id="middle">
+            
+            
+            
+            
+            
+            
+            
+            
+            
+        
