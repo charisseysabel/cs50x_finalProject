0054710b09280e0f5502eb27d398641de80ef4a5
Charisse <charisse.ysabel930@gmail.com>
2015-11-15 01:36:22 +0100

    Greet user by its registered username

--- public/dashboard.php
+++ public/dashboard.php
@@ -7,7 +7,17 @@
 
 	// configuration
 	require("../includes/config.php");
-	require("../includes/render-dash.php");
+	require("../includes/render_dash.php");
 
+    // if user reached page via GET...
+	if($_SERVER["REQUEST_METHOD"] == "GET")
+	{
+	    $queryUsers = query("SELECT username FROM users WHERE id = ?", $_SESSION["id"] );
+	    $gotUser = $queryUsers[0]["username"];
+	    
+	    
+		// render the custom hompage HTML
+		render("dash_main.php", ["title" => "Dashboard", "user" => $gotUser ] );
+	}
 
 ?>
