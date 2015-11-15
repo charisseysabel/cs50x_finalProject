d9d3b4d155693859d2f16157939f42f3483e6913
Charisse <charisse.ysabel930@gmail.com>
2015-11-15 01:40:43 +0100

    Change something I can't remember

--- templates/header-homepage.php
+++ templates/header-homepage.php
@@ -2,10 +2,16 @@
 <html>
 	<head>
 
-		<!-- various CSS files/libraries -->
+		<!-- various libraries -->
 		<link href="/css/bootstrap.min.css" rel="stylesheet"/>
         <link href="/css/bootstrap-theme.min.css" rel="stylesheet"/>
+
+        <!-- CSS -->
+        <link href="/css/homepage.css" rel="stylesheet"/>
+
+        <!-- fonts -->
         <link href='https://fonts.googleapis.com/css?family=Ubuntu' rel='stylesheet' type='text/css'>
+        <link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro' rel='stylesheet' type='text/css'>
 
         <!-- set the title dynamically through PHP -->
         <?php if(isset($title)): ?>
@@ -16,6 +22,7 @@
 
     	<!-- scripts -->
     	<script src="/js/jquery-1.11.1.min.js"></script>
+        <script src="/js/jquery.leanModal.min.js"></script>
         <script src="/js/bootstrap.min.js"></script>
 
 	</head>
