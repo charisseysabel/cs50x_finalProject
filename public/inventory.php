6412e9675d13041fb422e3c14e1d64d487bd34d3
Charisse <charisse.ysabel930@gmail.com>
2015-11-15 01:49:09 +0100

    Add to render the inventory tab

---
+++ public/inventory.php
@@ -0,0 +1,18 @@
+<?php
+	/**
+	 *	inventory.php
+	 *	configures the inventory page.
+	 */
+	
+	// configuration
+	require("../includes/config.php");
+	require("../includes/render_dash.php");
+	
+	// if user reached page via GET...
+	if($_SERVER["REQUEST_METHOD"] == "GET")
+	{
+		// render the custom hompage HTML
+		render("inv_main.php", ["title" => "Inventory" ] );
+	}
+
+?>
