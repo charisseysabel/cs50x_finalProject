de9bc12e1c59f92f40341f1e575e6677ba883e31
Charisse <charisse.ysabel930@gmail.com>
2015-11-16 14:16:21 +0100

    Add cashflow tab with interactive charts

---
+++ templates/cash_main.php
@@ -0,0 +1,10 @@
+<!--
+    cash_main.php
+    main content for the cashflow tab (HTML)
+-->
+
+
+<!-- Create the chart here -->
+<h1>Cashflow</h1>
+
+<canvas id="myChart" width="600" height="400"> </canvas>
