c6d745fbf45056258a073fd75c7aff745d513627
Charisse <charisse.ysabel930@gmail.com>
2015-11-18 15:04:33 +0100

    Add functionality to the settings page

--- public/settings.php
+++ public/settings.php
@@ -14,5 +14,44 @@
 		// render the custom hompage HTML
 		render("settings_main.php", ["title" => "Settings" ] );
 	}
+	
+	else if($_SERVER["REQUEST_METHOD"] == "POST")
+	{
+            // check for other errors
+			if(empty($_POST["password"]) )
+			{
+				apologize("Please enter your old password.");
+			}
+			else if(empty($_POST["new_pw"]) )
+			{
+				apologize("Please enter a new password.");
+			}
+			else if(empty($_POST["confirmation"]) )
+			{
+				apologize("Please confirm your new password.");
+			}
+			else if(($_POST["new_pw"]) !== ($_POST["confirmation"]) )
+			{
+				apologize("New password inputs do not match.");
+			}
+			else if($_POST["new_pw"] === ($_POST["password"]) )
+			{
+				apologize("Old and new password match. Please try another one.");
+			}
+				
+		// if input passes checkpoint...
+			else if((!empty($_POST["password"])) && ($_POST["new_pw"] === $_POST["confirmation"]) )
+			{
+				$update_pw = query("UPDATE users SET hash = ? WHERE id = ?", crypt($_POST["new_pw"]), $_SESSION["id"] );
+				if($update_pw === false)
+				{
+					apologize("Error updating password");
+				}	
+			}
+		
+		redirect("settings.php");
+		   
+        }
+	    
 
 ?>
