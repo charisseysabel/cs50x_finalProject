1de64e175c62e95107a01e1ed7df81a3e55fe4da
Charisse <charisse.ysabel930@gmail.com>
2015-11-18 15:10:31 +0100

    Modify

--- templates/inv_main.php
+++ templates/inv_main.php
@@ -3,51 +3,76 @@
     main content for the inventory tab (HTML)
 -->
 
-<h1>Inventory</h1>
+<div class="mid">
+    <h1>Inventory</h1>
 
-<!-- search  -->
-    <form class="search_form" id="form" role="form">
+    <!-- search  -->
+        <form class="search_form" id="form" role="form">
 
-            <label class="sr-only" for="q">Search...</label>
-            <input class="form-control" id="q" placeholder="Search..." type="text"/>
+                <label class="sr-only" for="q">Search...</label>
+                <input class="form-control" id="q" placeholder="Search..." type="text"/>
 
-    </form>
+        </form>
 
-<!-- table buttons -->
+    <!-- table buttons -->
 
-    <div class="tbl_btn">
-        <a href="add_form.php" class="btn btn-default" type="button">Add</a>
-        <a href="del_form.php" class="btn btn-default">Remove</a>
-    </div>
-        
+        <div class="tbl_btn">
+            <a href="add_form.php" class="btn btn-default" type="button">Add</a>
+            <a href="del_form.php" class="btn btn-default">Delete</a>
 
-<!-- TABLE -->
+        </div>
+            
 
-    <table class="table table-striped">
-	    <thead>
-		    <tr>
-			    <th>Product Name</th>
-			    <th>Unit Price</th>
-			    <th>Retail Price</th>
-			    <th>Supplier Name</th>			
-			    <th>Contact Number</th>
-			    <th>Category</th>
-		    </tr>
-	    </thead>
+    <!-- TABLE -->
+
+        <table class="table table-striped">
+	        <thead>
+		        <tr>
+			        <th>Product Name</th>
+			        <th>Unit Price</th>
+			        <th>Retail Price</th>
+			        <th>Supplier Name</th>			
+			        <th>Contact Number</th>
+			        <th>Category</th>
+		        </tr>
+	        </thead>
 	
-	    <tbody>
-	        <?php foreach($allInfo as $info): ?>
-	            <tr>
-	                <td> <?= $info["product"] ?> </td>
-	                <td>$ <?= $info["unit_price"] ?> </td>
-	                <td>$ <?= $info["retail_price"] ?> </td>
-	                <td> <?= $info["supplier_name"] ?> </td>
-	                <td> <?= $info["supplier_contact"] ?> </td>
-	                <td> <?= $info["category"] ?> </td>
-	            </tr>
-	        <?php endforeach ?>
+	        <tbody>
+	            <?php foreach($allInfo as $info): ?>
+	                <tr>
+	                    <td> <?= $info['product'] ?> </td>
+	                    <td>$ <?= $info["unit_price"] ?> </td>
+	                    <td>$ <?= $info["retail_price"] ?> </td>
+	                    <td> <?= $info["supplier_name"] ?> </td>
+	                    <td> <?= $info["supplier_contact"] ?> </td>
+	                    <td> <?= $info["category"] ?> </td>
+	                    <td> <input type="checkbox" name="checkbox[]" value="<?= $info['product'] ?>"/> </td>
+	                </tr>
+	            <?php endforeach ?>
 
 
-	    </tbody>
-    </table>
+	        </tbody>
+        </table>
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- templates/new_item.php
+++ templates/new_item.php
@@ -2,44 +2,44 @@
     new_item.php
     form for adding a new item into inventory
 -->
+<div class="mid">
+    <h1>Add new item</h1>
+    <p><i>All fields required</i></p>
 
-<h1>Add new item</h1>
-<p><i>All fields required</i></p>
 
-
-<form action="add_form.php" method="post">
-	<fieldset>
-		<div class="form-group">
-			<input autofocus class="form-control" name="productName" placeholder="Product name" type="text"/>
-		</div>
-		<div class="form-group">
-			<input class="form-control" name="unitPrice" placeholder="Unit price" type="text"/>
-		</div>
-		<div class="form-group">
-			<input class="form-control" name="retailPrice" placeholder="Retail price" type="text"/>
-		</div>
-		<div class="form-group">
-		    <select class="form-control" name="category">
-		        <option disabled selected value>Category</option>
-		        <option value="caffeteria">Caffeteria</option>
-		        <option value="food">Food</option>
-		        <option value="drinks">Drinks</option>
-		    </select>
-		</div>
+    <form action="add_form.php" method="post">
+	    <fieldset>
+		    <div class="form-group">
+			    <input autofocus class="form-control" name="productName" placeholder="Product name" type="text"/>
+		    </div>
+		    <div class="form-group">
+			    <input class="form-control" name="unitPrice" placeholder="Unit price" type="text"/>
+		    </div>
+		    <div class="form-group">
+			    <input class="form-control" name="retailPrice" placeholder="Retail price" type="text"/>
+		    </div>
+		    <div class="form-group">
+		        <select class="form-control" name="category">
+		            <option disabled selected value>Category</option>
+		            <option value="caffeteria">Caffeteria</option>
+		            <option value="food">Food</option>
+		            <option value="drinks">Drinks</option>
+		        </select>
+		    </div>
 		
 		
-		<div class="form-group">
-			<input class="form-control" name="supName" placeholder="Supplier's name" type="text"/>
-		</div>
-		<div class="form-group">
-			<input class="form-control" name="supNumber" placeholder="Supplier's number" type="text"/>
-		</div>
+		    <div class="form-group">
+			    <input class="form-control" name="supName" placeholder="Supplier's name" type="text"/>
+		    </div>
+		    <div class="form-group">
+			    <input class="form-control" name="supNumber" placeholder="Supplier's number" type="text"/>
+		    </div>
 		
-		<div class="form-group">
-			<button type="submit" class="btn btn-default">Add</button>
-		</div>
-	</fieldset>
-</form>
+		    <div class="form-group">
+			    <button type="submit" class="btn btn-default">Add</button>
+		    </div>
+	    </fieldset>
+    </form>
 
-
+</div>
 
