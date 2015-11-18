137d523f87b8036662da8b7ac027c98c44147e3c
Charisse <charisse.ysabel930@gmail.com>
2015-11-18 15:05:52 +0100

    Add forms to modify user account

--- templates/settings_main.php
+++ templates/settings_main.php
@@ -2,5 +2,47 @@
     settings_main.php
     user interface for the settings tab (HTML)    
 -->
-
-settings page
+<div class="mid">
+    <h1>Settings</h1>
+    
+    <div class="content">        
+        <p></p>
+        <div class="mod_pw">
+            <h4>Change your password</h4>
+            <form action="settings.php" method="post">
+	            <fieldset>
+		            <div class="form-group">
+			            <input class="form-control" name="password" placeholder="Current password" type="password"/>
+		            </div>
+		            <div class="form-group">
+			            <input class="form-control" name="new_pw" placeholder="New password" type="password"/>
+		            </div>
+		            <div class="form-group">
+			            <input class="form-control" name="confirmation" placeholder="Confirm new password" type="password"/>
+		            </div>
+		            <div class="form-group">
+			            <button type="submit" class="btn btn-default">Change</button>
+		            </div>
+	            </fieldset>
+            </form>
+        </div>
+        
+        <div class="del_acc">
+            <h4>Delete account</h4>
+            <p>Warning: This cannot be undone!</p>
+            <form action="del_all.php" method="post">
+	            <fieldset>
+		            <div class="form-group">
+			            <input class="form-control" name="username" placeholder="Username" type="text"/>
+		            </div>
+		            <div class="form-group">
+			            <input class="form-control" name="password" placeholder="Password" type="password"/>
+		            </div>
+		            <div class="form-group">
+			            <button type="submit" class="btn btn-default">Delete account</button>
+		            </div>
+	            </fieldset>
+            </form>
+        </div>
+    </div>
+</div>
