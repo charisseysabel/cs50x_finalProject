d256ed7073323c6ff0e9159d82459cf0a9a54d0f
Charisse <charisse.ysabel930@gmail.com>
2015-11-18 15:07:05 +0100

    Add functionality to settings page

--- public/del_form.php
+++ public/del_form.php
@@ -1,18 +1,37 @@
 <?php
 	/**
 	 *	del_form.php
-	 *	configures the cashflow page.
+	 *	deletes an item from the database through the checkbox.
 	 */
 	
-	// configuration
-	require("../includes/config.php");
-	require("../includes/render_dash.php");
+    require("../includes/config.php");
 	
-	// if user reached page via GET...
-	if($_SERVER["REQUEST_METHOD"] == "GET")
-	{
-		// render the custom hompage HTML
-		render(".php", ["title" => "Delete item" ] );
-	}
+    if(isset($_GET['checkbox']) )
+    {
+        foreach($_GET['checkbox'] as $del_id )
+        {
+            $delete = query("DELETE FROM 'inventory' WHERE id = '?' AND 'product'='?' ", $_SESSION["id"], $del_id);
+            if($delete === false)
+            {
+                apologize("sorry");
+            }
+        }
+        redirect("inventory.php");
+    }
+
+
+
+
+/*  CREATE TABLE `inventory` (
+ `id` int(10) NOT NULL,
+ `product` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+ `unit_price` decimal(65,0) NOT NULL,
+ `retail_price` decimal(65,0) NOT NULL,
+ `supplier_name` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+ `supplier_contact` int(20) NOT NULL,
+ `category` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+ UNIQUE KEY `product` (`product`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci
+    */
 
 ?>
