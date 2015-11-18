992a1a404f3dcca738506001bc6f243fdd24ad2d
Charisse <charisse.ysabel930@gmail.com>
2015-11-18 15:05:17 +0100

    Improve UI

--- templates/dash_main.php
+++ templates/dash_main.php
@@ -3,5 +3,17 @@
 	main content for the dashboard tab (HTML)
 -->
 
-	<h1>Hello, <?= htmlspecialchars($user)?>!</h1>
 
+    <div class="image">
+        <img src="/img/alberta_by_alex.jpg"/>
+    </div>
+    
+    <div class="greeting">
+        <h2>Hello, <?= htmlspecialchars($user)?>!</h2>  
+        <p id="date">&nbsp;</p>  
+        <span id="clock">&nbsp;</span>
+    </div>
+
+
+    
+    
