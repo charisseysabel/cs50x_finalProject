0216f703ccc9c7736d9ccc6d2c8ebaf65b27c063
Charisse <charisse.ysabel930@gmail.com>
2015-11-20 10:40:00 +0100

    Add div that will change based on ajax category filter

--- templates/inv_main.php
+++ templates/inv_main.php
@@ -5,74 +5,57 @@
 
 <div class="mid">
     <h1>Inventory</h1>
+        <div class="content">
+            <!-- table buttons -->
+                <div class="tbl_btn">
+                    <a href="add_form.php" class="btn btn-default" type="button">Add</a>
+                    <input type="button" value="Delete" class="btn btn-default" id="delete" name="delete" formaction="inventory.php"/>
+                    
+                    <select class="form-control" name="filter" id="filter" onchange="setFilter(this.value);">
+		                    <option disabled selected value>Filter</option>
+		                    <option value="caffe_ajax">Caffeteria</option>
+		                    <option value="food_ajax">Food</option>
+		                    <option value="drinks_ajax">Drinks</option>
+		            </select>                   
+                </div>
+        
+            <!-- search  -->
+                <form class="search_form" id="search" name="search" role="form" action="">
+                   <!--     <label class="sr-only" for="q">Search...</label> -->
+                        <input class="form-control" id="txtSearch" name="txtSearch" maxlength="50" placeholder="Search..." type="text"/>                       
+                </form>
 
-    <!-- search  -->
-        <form class="search_form" id="form" role="form">
-
-                <label class="sr-only" for="q">Search...</label>
-                <input class="form-control" id="q" placeholder="Search..." type="text"/>
-
-        </form>
-
-    <!-- table buttons -->
-
-        <div class="tbl_btn">
-            <a href="add_form.php" class="btn btn-default" type="button">Add</a>
-            <a href="del_form.php" class="btn btn-default">Delete</a>
-
-        </div>
-            
-
-    <!-- TABLE -->
-
-        <table class="table table-striped">
-	        <thead>
-		        <tr>
-			        <th>Product Name</th>
-			        <th>Unit Price</th>
-			        <th>Retail Price</th>
-			        <th>Supplier Name</th>			
-			        <th>Contact Number</th>
-			        <th>Category</th>
-		        </tr>
-	        </thead>
+            <!-- TABLE -->
+                <div id="inv_tbl">
+                
+                    <table class="table table-striped" id="tblSearch">
+	                    <thead>
+		                    <tr>
+			                    <th>Product Name</th>
+			                    <th>Unit Price</th>
+			                    <th>Retail Price</th>
+			                    <th>Supplier Name</th>			
+			                    <th>Category</th>
+		                    </tr>
+	                    </thead>
 	
-	        <tbody>
-	            <?php foreach($allInfo as $info): ?>
-	                <tr>
-	                    <td> <?= $info['product'] ?> </td>
-	                    <td>$ <?= $info["unit_price"] ?> </td>
-	                    <td>$ <?= $info["retail_price"] ?> </td>
-	                    <td> <?= $info["supplier_name"] ?> </td>
-	                    <td> <?= $info["supplier_contact"] ?> </td>
-	                    <td> <?= $info["category"] ?> </td>
-	                    <td> <input type="checkbox" name="checkbox[]" value="<?= $info['product'] ?>"/> </td>
-	                </tr>
-	            <?php endforeach ?>
+	                    <tbody>
+	                        <?php foreach($allInfo as $info): ?>
+	                            <tr>
+	                                <td> <?= $info['product'] ?> </td>
+	                                <td>$ <?= $info["unit_price"] ?> </td>
+	                                <td>$ <?= $info["retail_price"] ?> </td>
+	                                <td> <?= $info["supplier_name"] ?> </td>
+	                                <td> <?= $info["category"] ?> </td>
+	                                <td> <input type="checkbox" id="checkbox[]" name="checkbox[]" value="<?= $info['product'] ?>"/> </td>
+	                            </tr>
+	                        <?php endforeach ?>
 
 
-	        </tbody>
-        </table>
-
+	                    </tbody>
+                    </table>
+              </div>
+        </div>
 </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
