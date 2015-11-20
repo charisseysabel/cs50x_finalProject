ffd54e40fd39f35296e1b8e2c62d454f0f99094e
Charisse <charisse.ysabel930@gmail.com>
2015-11-20 10:39:18 +0100

    Remove extra js file for inventory tab then added it to main script file

--- templates/header-dashboard.php
+++ templates/header-dashboard.php
@@ -27,6 +27,7 @@
     	<script src="/js/jquery-1.11.1.min.js"></script>
         <script src="/js/bootstrap.min.js"></script>
         <script src="/js/scripts.js"></script>
+
         
       <!-- CASHFLOW TAB: chart.js by Nick Downie -->
       <script src="/js/Chart.js"></script>
