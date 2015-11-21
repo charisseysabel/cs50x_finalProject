c5bec0d708d259553ee91ce2ea10fab68323a44a
Charisse <charisse.ysabel930@gmail.com>
2015-11-22 00:06:24 +0100

    Add transactions tab functionality

--- public/transactions.php
+++ public/transactions.php
@@ -11,8 +11,22 @@
 	// if user reached page via GET...
 	if($_SERVER["REQUEST_METHOD"] == "GET")
 	{
+	    // get all data in transactions database
+	    $trans_all = [];
+	    $getTrans = query("SELECT * FROM transactions WHERE id = ?", $_SESSION["id"] );
+	    foreach($getTrans as $data)
+	    {
+	        $trans_all[] = [
+	            "trans_name" => $data["trans_name"],
+	            "trans_category" => $data["trans_category"],
+	            "trans_sub_cat" => $data["trans_sub_cat"],
+	            "trans_amount" => number_format($data["trans_amount"], 2),
+	            "trans_time" => $data["trans_time"]
+	        ];
+	    }
+	
 		// render the custom hompage HTML
-		render("trans_main.php", ["title" => "Transactions" ] );
+		render("trans_main.php", ["title" => "Transactions", "trans_all" => $trans_all ] );
 	}
 
 ?>
