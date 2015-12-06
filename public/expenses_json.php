bf10db6728d1b115f8d18a732ecc61c2b8e1d1b5
Charisse <charisse.ysabel930@gmail.com>
2015-12-06 17:42:24 +0100

    Modify date json format

--- public/expenses_json.php
+++ public/expenses_json.php
@@ -11,12 +11,21 @@
 	
 	// search database
 	$getExp = query("SELECT trans_time, trans_name, trans_sub_cat, trans_amount FROM `transactions` WHERE `id` = ? AND trans_category = 'expense'", $_SESSION["id"]);
-	if($getExp !== false)
+	if($getExp === false)
 	{
-	    $exp_json = $getExp;
+	    apologize("Error fetching income transactions");
+	}
 	
+	foreach($getExp as $value)
+	{
+	    $exp_json[] = [
+	        "trans_name" => $value["trans_name"],
+	        "trans_sub_cat" => $value["trans_sub_cat"],
+	        "trans_amount" => $value["trans_amount"],
+	        "trans_time" => date('M Y', strtotime($value["trans_time"]))
+	    ];
+	}
 	    // output as JSON
 	    header("Content-type: application/json");
 	    print(json_encode($exp_json, JSON_PRETTY_PRINT));
-	}
 ?>
--- public/income_json.php
+++ public/income_json.php
@@ -11,12 +11,23 @@
 	
 	// search database
 	$search = query("SELECT trans_name, trans_sub_cat, trans_amount, trans_time FROM `transactions` WHERE `id` = ? AND trans_category = 'income'", $_SESSION["id"]);
-	if($search !== false)
+	if($search === false)
 	{
-	    $users = $search;
+	    apologize("Error fetching income transactions");
+	}
 	
+	foreach($search as $value)
+	{
+	    $users[] = [
+	        "trans_name" => $value["trans_name"],
+	        "trans_sub_cat" => $value["trans_sub_cat"],
+	        "trans_amount" => $value["trans_amount"],
+	        "trans_time" => date('M Y', strtotime($value["trans_time"]))
+	    ];
+	}
+		
 	    // output as JSON
 	    header("Content-type: application/json");
 	    print(json_encode($users, JSON_PRETTY_PRINT));
-	}
+	
 ?>
